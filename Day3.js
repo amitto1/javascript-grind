e0cee5f7bde7be91bf7295655759a4e964218951
amitto1 <amitkp3001@gmail.com>
2026-06-24 08:58:16 +0530

    add Q9 and Q10

--- Day3.js
+++ Day3.js
@@ -25,10 +25,63 @@ var compose = function(functions) {
     
     return function(x) {
         let ans = x;
-        // if(functions.length <=1 ) return x;
+        if(functions.length === 0) return x;
         for(let i = functions.length - 1; i >= 0 ; i--){
           ans = functions[i](ans);
         }
         return ans;
+    }
+};
+
+/* Q9
+Write a function argumentsLength that returns the count of arguments passed to it.
+ 
+
+Example 1:
+
+Input: args = [5]
+Output: 1
+Explanation:
+argumentsLength(5); // 1
+
+One value was passed to the function so it should return 1.
+Example 2:
+
+Input: args = [{}, null, "3"]
+Output: 3
+Explanation: 
+argumentsLength({}, null, "3"); // 3
+
+Three values were passed to the function so it should return 3.
+ */
+
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/*Q10
+Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+The first time the returned function is called, it should return the same result as fn.
+Every subsequent time it is called, it should return undefined.
+ 
+Example 1:
+
+Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+Output: [{"calls":1,"value":6}]
+Explanation:
+const onceFn = once(fn);
+onceFn(1, 2, 3); // 6
+onceFn(2, 3, 6); // undefined, fn was not called
+*/
+
+var once = function(fn) {
+    let hasBeenCalled = false;
+    return function(...args){
+        if(hasBeenCalled){
+            return undefined;
+        } 
+        hasBeenCalled = true;
+        return fn(...args);
     }
 };
