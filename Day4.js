f22d9f99c52f910da12c71b044ce10c0d12d3fe5
amitto1 <amitkp3001@gmail.com>
2026-06-27 13:04:05 +0530

    adding soln to problem 14 & 15

--- Day4.js
+++ Day4.js
@@ -17,7 +17,7 @@ var addTwoPromises = async function (promise1, promise2) {
     const res2 = await promise2;
     // return res1 + res2;
 
-    const ans =  new Promise ((resolve, reject) => {
+    const ans = new Promise((resolve, reject) => {
         resolve(res1 + res2);
     })
 
@@ -50,3 +50,108 @@ async function sleep(millis) {
     return new Promise(resolve => setTimeout(resolve, millis));
 }
 
+/* Q14
+Given a function fn, an array of arguments args, and a timeout t in milliseconds, return a cancel function cancelFn.
+
+After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.
+
+setTimeout(cancelFn, cancelTimeMs)
+Initially, the execution of the function fn should be delayed by t milliseconds.
+
+If, before the delay of t milliseconds, the function cancelFn is invoked, it should cancel the delayed execution of fn. Otherwise, if cancelFn is not invoked within the specified delay t, fn should be executed with the provided args as arguments.
+
+Example 1:
+
+Input: fn = (x) => x * 5, args = [2], t = 20
+Output: [{"time": 20, "returned": 10}]
+Explanation: 
+const cancelTimeMs = 50;
+const cancelFn = cancellable((x) => x * 5, [2], 20);
+setTimeout(cancelFn, cancelTimeMs);
+
+The cancellation was scheduled to occur after a delay of cancelTimeMs (50ms), which happened after the execution of fn(2) at 20ms.
+*/
+
+var cancellable = function (fn, args, t) {
+    const timerId = setTimeout(() => {
+        fn(...args);
+    }, t);
+    return function cancelFn() {
+        clearTimeout(timerId);
+    }
+
+};
+
+/* Q15
+Given a function fn, an array of arguments args, and an interval time t, return a cancel function cancelFn.
+
+After a delay of cancelTimeMs, the returned cancel function cancelFn will be invoked.
+
+setTimeout(cancelFn, cancelTimeMs)
+The function fn should be called with args immediately and then called again every t milliseconds until cancelFn is called at cancelTimeMs ms.
+
+Example 1:
+
+Input: fn = (x) => x * 2, args = [4], t = 35
+Output: 
+[
+   {"time": 0, "returned": 8},
+   {"time": 35, "returned": 8},
+   {"time": 70, "returned": 8},
+   {"time": 105, "returned": 8},
+   {"time": 140, "returned": 8},
+   {"time": 175, "returned": 8}
+]
+Explanation: 
+const cancelTimeMs = 190;
+const cancelFn = cancellable((x) => x * 2, [4], 35);
+setTimeout(cancelFn, cancelTimeMs);
+
+Every 35ms, fn(4) is called. Until t=190ms, then it is cancelled.
+1st fn call is at 0ms. fn(4) returns 8.
+2nd fn call is at 35ms. fn(4) returns 8.
+3rd fn call is at 70ms. fn(4) returns 8.
+4th fn call is at 105ms. fn(4) returns 8.
+5th fn call is at 140ms. fn(4) returns 8.
+6th fn call is at 175ms. fn(4) returns 8.
+Cancelled at 190ms
+ */
+
+var cancellable = function (fn, args, t) {
+    fn(...args);
+    const intervalId = setInterval(() => {
+        fn(...args);
+    }, t);
+    return function cancelFn() {
+        clearInterval(intervalId);
+    }
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *   
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)    
+ */
