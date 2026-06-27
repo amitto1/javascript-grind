d23535bd2e9219c409c6b1269041ee923e69723a
amitto1 <amitkp3001@gmail.com>
2026-06-27 12:42:37 +0530

    add problem 13 to day4

--- Day4.js
+++ Day4.js
@@ -24,3 +24,29 @@ var addTwoPromises = async function (promise1, promise2) {
     return ans;
 };
 
+/* Q13
+Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+Note that minor deviation from millis in the actual sleep duration is acceptable.
+
+Example 1:
+
+Input: millis = 100
+Output: 100
+Explanation: It should return a promise that resolves after 100ms.
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // 100
+});
+*/
+
+async function sleep(millis) {
+    // return new Promise((resolve, reject) => {
+    //     setTimeout(() => {
+    //         resolve();
+    //     }, millis);
+    // })
+
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+
